fab35b19e2eaf205f493735902a5e0cdf55b48e3
vanshkapoor <vanshkapoorvk7@gmail.com>
2019-06-21 14:05:54 +0530

    server file changes

--- server.js
+++ server.js
@@ -40,14 +40,14 @@ app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
 // server static assets if in production
-if(process.env.NODE_ENV === 'production' ){
-	//set static folder
-	app.use(express.static('client/build'));
+// if(process.env.NODE_ENV === 'production' ){
+// 	//set static folder
+// 	app.use(express.static('client/build'));
 
-	app.get('*',(req,res)=>{
-		res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-	});
-}
+// 	app.get('*',(req,res)=>{
+// 		res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+// 	});
+// }
 	
 
 //serving
